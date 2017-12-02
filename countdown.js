d199d6b1c3c71adb5bfc83acbfe7b6918cf499a0
xmanatee <xmanatee.one@gmail.com>
2017-12-03 00:15:18 +0300

    base webpage

---
+++ countdown.js
@@ -0,0 +1,38 @@
+function pad(n) {
+    return (n < 10) ? ("0" + n) : n;
+}
+
+const getCountdown = function(elemId, countDownDate) {
+    return function() {
+        var now = new Date().getTime();
+
+        var distance = countDownDate - now;
+
+        var days = Math.floor(distance / (1000 * 60 * 60 * 24));
+        var hours = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+        var minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
+        var seconds = Math.floor((distance % (1000 * 60)) / 1000);
+
+        document.getElementById(elemId).innerHTML =
+            pad(days) + ":" +
+            pad(hours) + ":" +
+            pad(minutes) + ":" +
+            pad(seconds) + "";
+
+        if (distance < 0) {
+            clearInterval(x);
+            document.getElementById(elemId).innerHTML = "EXPIRED";
+        }
+    };
+};
+
+const countDownDate = new Date("Dec 9, 2017 00:00:00").getTime();
+
+const countdownFunc = getCountdown("countdown_p", countDownDate);
+
+
+const x = setInterval(countdownFunc, 1000);
+
+window.onreadystatechange = function() {
+    countdownFunc();
+};
---
+++ main.js
@@ -0,0 +1,9 @@
+// window.onload = function() {
+//     console.log("lalaka");
+//     document.getElementById("title").style.visibility = "hidden"; //.setAttribute("visibility", "hidden");
+//     setTimeout(function() {
+//         console.log("lalakasa");
+//         document.getElementById("title").style.visibility = "visible"; // .setAttribute("visibility", "visible");
+//     }, 2000);
+//
+// };
